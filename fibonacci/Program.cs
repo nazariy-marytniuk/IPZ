2924f683895d0b58b3358e50a15308e3a7e9999c
nazariy-martyniuk <nazya19.martvad@gmail.com>
2024-09-16 12:39:44 +0300

    Завдання 2. Лаб1

---
+++ fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace fibonacci
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci();
+        }
+
+        public static void Fibonacci()
+        {
+            uint temp;
+            uint number = 1;
+
+            Console.Write("Count -> ");
+
+            if (!int.TryParse(Console.ReadLine(), out int numberOfElements))
+            {
+                Console.WriteLine("Not a number");
+                Environment.Exit(0);
+            }
+
+            temp = number;
+
+            Console.Write(number.ToString() + " ");
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.Write(number.ToString() + " ");
+                number = number + temp;
+                temp = number - temp;
+            }
+        }
+    }
+}
